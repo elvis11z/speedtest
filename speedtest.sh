600708369349fb1c820d77b280a87b94bfe82a67
elvis11z <elvis11z@gmail.com>
2022-08-05 17:57:50 -0500

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /root
+#ver la ultima version en http://ftp.de.debian.org/debian/pool/main/s/speedtest-cli/
 wget http://ftp.de.debian.org/debian/pool/main/s/speedtest-cli/speedtest-cli_2.1.3-2_all.deb
 dpkg --install speedtest-cli_2.1.3-2_all.deb
 echo "speedtest instalado"
